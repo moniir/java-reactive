4bef1d3c70fc8d7f9c00a5cb29d4279ffdf1420e
Monir Hossain <monir198@gmail.com>
2023-05-27 19:02:19 +0600

    Updated default subscriber class

--- src/main/java/com/rp/reactor/DefaultSubscriberDemo.java
+++ src/main/java/com/rp/reactor/DefaultSubscriberDemo.java
@@ -3,15 +3,21 @@
  */
 package com.rp.reactor;
 
+import com.github.javafaker.Faker;
 import com.rp.courseutil.Util;
 import reactor.core.publisher.Flux;
 
 public class DefaultSubscriberDemo {
     public static void main(String[] args) {
         Flux.create(fluxSink -> {
-            for (int i=0;i<10;i++){
+            /*for (int i=0;i<10;i++){
                 fluxSink.next(Util.faker().country().name());
-            }
+            }*/
+            String countryName;
+            do {
+                countryName = Faker.instance().country().name();
+                fluxSink.next(countryName);
+            } while (!countryName.equalsIgnoreCase("Bangladesh"));
             fluxSink.complete();
         }).subscribe(Util.subscriber());
     }
